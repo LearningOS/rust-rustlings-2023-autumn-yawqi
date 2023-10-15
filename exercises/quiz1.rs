ac54191eedcfb660b598005671fb72b90a7aa18e
Qi Wang <mpiglet@outlook.com>
2023-10-15 23:13:21 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,12 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
 
 // Don't modify this function!
+fn calculate_price_of_apples(count: i32) -> i32 {
+    if (count > 40) {
+        count
+    } else {
+        2 * count
+    }
+}
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
